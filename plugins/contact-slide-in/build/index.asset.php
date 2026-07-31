b9e846c5fa44a5cda08092a67d43cdb8450b6f6b
Greg Rozmarynowycz <greg@thunderlab.net>
2026-07-31 17:39:42 +0200

    remove original plugin attempt

--- plugins/contact-slide-in/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element'), 'version' => '4da2c0a925484d56a8df');
--- plugins/contact-slide-in/templates/app.php
+++
@@ -1,3 +0,0 @@
-<div id="contact-slide-in">
-    <h2>Loading... test test</h2>
-</div>
